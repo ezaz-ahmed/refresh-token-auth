2295cff9f946e66194783042bb2ab4818bfb79fd
Ezaz Ahmed <ezaz.dev@gmail.com>
2022-12-04 22:14:45 +0600

    ✔: Client done

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom"
 
+import Users from './Users'
+
 const Admin = () => {
     return (
         <section>
             <h1>Admins Page</h1>
             <br />
+            <Users />
             <p>You must have been assigned an Admin role.</p>
             <div className="flexGrow">
                 <Link to="/">Home</Link>
